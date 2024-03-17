fb6952d222542707b2219baa753cdb29b5e9a373
Marton Mack <mackmarton@edu.bme.hu>
2024-03-17 10:43:47 +0100

    Refactored render middleware.

--- site/middlewares/render.js
+++ site/middlewares/render.js
@@ -1,5 +1,5 @@
 /*Renders a page with the given template and data*/
-module.exports = (objectRepository) => {
+module.exports = (objectRepository, viewName) => {
     return (req, res, next) => {
         return next();
     };
